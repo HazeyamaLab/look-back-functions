64cc2c09a22523f6b41a74685a0503ed4be70836
HKT0131 <e175414y@st.u-gakugei.ac.jp>
2020-10-19 23:02:21 +0900

    実際にデータを読み込むようにする

--- src/functions/comments.js
+++ src/functions/comments.js
@@ -12,23 +12,32 @@ exports.handler = async function(event, context) {
     //日の取得
     var day = new Date();
 
-    const issue_number = 1;//=event.body.payload.issue
-    const fix = "修正"; //=event.body.payload.fix
-    const cause = "げんいん";//=event.body.payload.cause
-    const study = "学んだこと";//=event.body.payload.study
+    const issue_number =1;//=event.body.payload.issue
+    const fix =event.body.payload.fix;
+    const cause =event.body.payload.cause;
+    const study =event.body.payload.study;
     const date = (day.getMonth()+1)+"月"+day.getDate()+"日";
-    const human = "人";//=event.body.payload.human
+    const human =event.body.payload.human;
     // const repo = event.body.payload.repo
-    const body = "## 修正内容   "+fix+"   ## 原因   "+cause+"   ## 学んだこと   "+study+"   ## リリース日   "+date+"   ## 修正者   "+human;
+    const body = `## 修正内容
+`+fix+`
+## 原因
+`+cause+`
+## 学んだこと
+`+study+`
+## リリース日
+`+date+`
+## 修正者
+`+human;
     
     const data = await octokit.issues.createComment({
         owner: "HazeyamaLab",
         repo: "look-back-functions",
-        issue_number: 1,
-        body: "OKOK"
+        issue_number,
+        body
     })
     console.log(data)
-    
+
     return {
         statusCode: 200,
         body: JSON.stringify(event)
